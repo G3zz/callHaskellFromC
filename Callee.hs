90a17a1dba691471f56e73e6609881bc751b90e1
Geraint Ballinger <G3zz@users.noreply.github.com>
2019-02-02 13:52:06 +0000

    Update Callee.hs

--- Callee.hs
+++ Callee.hs
@@ -1,10 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Callee where
 
-import Control.Monad
-
 foreign export ccall helloFromHaskell :: IO ()
 
 helloFromHaskell = print "Hello C, from Haskell!"
-
-f = mapM (return Nothing) [0..3]
